b7fda465610f16ce7d3081443e176373edbf2bc4
Adit Rada <rada_adit@hotmail.com>
2025-04-01 20:50:31 -0400

    add generate fn

--- model/gpt.py
+++ model/gpt.py
@@ -198,6 +198,45 @@ def generate_text_simple(model, token_ids, max_new_tokens, context_size):
     return token_ids
 
 
+def generate(model, token_ids, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_id=None):
+
+    # For-loop is the same as before: Get logits, and only focus on last time step
+    for _ in range(max_new_tokens):
+        idx_cond = token_ids[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+
+        # New: Filter logits with top_k sampling
+        if top_k is not None:
+            # Keep only top_k values
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(logits < min_val, torch.tensor(float('-inf')).to(logits.device), logits)
+
+        # New: Apply temperature scaling
+        if temperature > 0.0:
+            logits = logits / temperature
+
+            # Apply softmax to get probabilities
+            probs = torch.softmax(logits, dim=-1)  # (batch_size, context_len)
+
+            # Sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (batch_size, 1)
+
+        # Otherwise same as before: get idx of the vocab entry with the highest logits value
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch_size, 1)
+
+        if idx_next == eos_id:  # Stop generating early if end-of-sequence token is encountered and eos_id is specified
+            break
+
+        # Same as before: append sampled index to the running sequence
+        token_ids = torch.cat((token_ids, idx_next), dim=1)  # (batch_size, num_tokens+1)
+
+    return token_ids
+
+
 def print_model_stats(model: GPTModel, model_name: str):
     print("Model Name:", model_name)
 
